618ffed25d3e95256c6047f8bddcb5819a463558
Pawan Siyag <pb299851@gmail.com>
2026-07-15 00:58:26 +0530

    add page4.html link in page3.js

---
+++ page3.js
@@ -0,0 +1,10 @@
+let mens = document.querySelector(".mens")
+
+mens.addEventListener("click" , (e) => {
+   e.preventDefault;
+   setTimeout(()=>{
+    window.location.href = "page4.html"
+   } ,1500);
+
+   
+});
